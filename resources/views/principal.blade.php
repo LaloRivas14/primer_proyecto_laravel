f2b4b902ed52f0eb7897c127da835d85b9f2ad93
LaloRivas14 <lautarorivas17@gmail.com>
2026-04-08 16:11:08 -0300

    prueba carrusel salio bien

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -1,4 +1,49 @@
 <!DOCTYPE html>
-<html lang = "es"><head>
-<link rel="stylesheet"href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
-<title>OZZY </title>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>OZZY</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
+</head>
+<body>
+    <div class="container mt-4">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            
+            <!-- Indicadores (los puntitos abajo) -->
+            <ol class="carousel-indicators">
+                <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>
+                <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
+                <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
+            </ol>
+
+            <!-- Slides -->
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-50" src="{{ asset('images/cap1.png') }}" alt="Primera imagen">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-50" src="{{ asset('images/cap2.png') }}" alt="Segunda imagen">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-50" src="{{ asset('images/cap3.png') }}" alt="Tercera imagen">
+                </div>
+            </div>
+
+            <!-- Controles (flechas) -->
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Anterior</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Siguiente</span>
+            </a>
+        </div>
+    </div>
+
+    <!-- Bootstrap JS + dependencias -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+</body>
+</html>
