7b7b21343d3b29d2726f80fdfdf8f155546f0146
LaloRivas14 <lautarorivas17@gmail.com>
2026-04-11 03:44:42 -0300

    prueba imagenes para la principal

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -5,45 +5,25 @@
     <meta charset="UTF-8">
     <title>OZZY</title>
     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('CSS/estilos.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
 </head>
 <body>
     @include('partials.barra-nav')
     <h1 class="text-center mt-4">Bienvenidos a Ozzy Remeras</h1>
 
-    <div class="container mt-4 ">
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+    <div class="container mt-4">
+        <ol>
+            <li><img class="img" src="{{ asset('images/cap1.png') }}" alt="Primera imagen">
+            <h3 class="text-right mt-4">¿Quiénes somos?</h3></li>
             
-            <!-- Indicadores (los puntitos abajo) -->
-            <ol class="carousel-indicators">
-                <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>
-                <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
-                <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
-            </ol>
 
-            <!-- Slides -->
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="d-block w-50" src="{{ asset('images/cap1.png') }}" alt="Primera imagen">
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-50" src="{{ asset('images/cap2.png') }}" alt="Segunda imagen">
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-50" src="{{ asset('images/cap3.png') }}" alt="Tercera imagen">
-                </div>
-            </div>
 
-            <!-- Controles (flechas) -->
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Anterior</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Siguiente</span>
-            </a>
-        </div>
+
+
+            <li><img class="img" src="{{ asset('images/cap2.png') }}" alt="Segunda imagen"></li>
+            <li><img class="img" src="{{ asset('images/cap3.png') }}" alt="Tercera imagen"></li>
+        </ol>
     </div>
 
     <!-- Bootstrap JS + dependencias -->
